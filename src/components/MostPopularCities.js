b343a65947902d81888d16eb504d598dc7db4fe4
m4tiss <mateusz039@op.pl>
2024-03-26 17:34:27 +0100

    fix text

--- src/components/MostPopularCities.js
+++ src/components/MostPopularCities.js
@@ -17,7 +17,7 @@ const MostPopularCities = () => {
 
   return (
     <div className="w-full flex flex-col justify-center items-center py-5">
-      <h2 className=" text-black text-2xl my-10 font-bold">
+      <h2 className=" text-black text-center text-2xl my-10 font-bold">
         {t("mostPopularCities")}✔
       </h2>
       <div className="flex flex-wrap gap-10 w-10/12 justify-center items-center">
